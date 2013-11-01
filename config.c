fac3854a9c072355196a8209d30928b3f7de0461
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2013-11-01 15:30:50 +0100

    the modules are fetched from the config file and set in the according config struct fields

--- config.c
+++ config.c
@@ -4,6 +4,9 @@
 int get_server_config(s_config *c)
 {
 	config_t cfg;
+	config_setting_t *modules_setting, *module_setting;
+	unsigned int count, i;
+	const char *module_name;
 
 	config_init(&cfg);
 
@@ -19,7 +22,24 @@ int get_server_config(s_config *c)
 		&& config_lookup_string(&cfg, "api_prefix", &(c->api_prefix))
 		&& config_lookup_string(&cfg, "index_file", &(c->index_file))
 		&& config_lookup_int(&cfg, "buffer_size", &(c->buffer_size))
+		&& config_lookup_int(&cfg, "api_modules_number", &(c->api_modules_number))
 	) {
+
+		/* Output a list of all movies in the inventory. */
+		modules_setting = config_lookup(&cfg, "api_modules");
+		if (modules_setting != NULL) {
+			count = (unsigned int) config_setting_length(modules_setting);
+
+			char *modules[count];
+			for (i = 0; i < count; ++i) {
+				module_setting = config_setting_get_elem(modules_setting, i);
+				module_name = config_setting_get_string(module_setting);
+
+				modules[i] = (char *) module_name;
+			}
+
+			c->api_modules = modules;
+		}
 		return CONFIG_FILE_READ_OK;
 	}
 	else
